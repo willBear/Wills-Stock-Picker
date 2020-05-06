1571b92cf51587519646362ee969516ce012f4c4
Will Xiong <will_xiong@hotmail.com>
2020-05-06 13:15:36 -0700

    Added webscraping from beautiful soup for price
    TODO:
    Add functionality for technicals
    Add functionality for price targets

--- Web_Scraping.py
+++ Web_Scraping.py
@@ -1,3 +1,22 @@
-###
+###########################
 # Created on May 6th 2020
-# 
+# @ Author: WillBear
+###########################
+
+
+
+import requests
+from bs4 import BeautifulSoup
+
+def find_stock_price():
+    url = 'https://finance.yahoo.com/quote/FB?p=FB'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text,'lxml')
+    # print(soup)
+    # The price is pulled from
+    price = soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
+
+    print(price)
+
+def find_analyst_target(symbol):
+    url = 'https://www.tradingview.com/symbols/NYSE-BA/technicals/'
